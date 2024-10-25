d585ab3da6db2eab1960a096f9c4b7c8d052f6c2
Lucas Azevedo <85646897+LucasAzevas@users.noreply.github.com>
2024-10-25 17:11:38 +0000

    alterando msg da página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Boa viagem Padrin, Brilha! zika zika</h1>;
+  return <h1>Em Construção</h1>;
 }
 
 export default Home;
+
+function teste() {
+  console.log("teste");
+}
