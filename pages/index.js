5e15c49e3d7a4f48c15b14d875d041504be5092a
Lucas Azevedo <85646897+LucasAzevas@users.noreply.github.com>
2024-10-25 21:16:22 +0000

    atualização pagina em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-  return <h1>Em Construção</h1>;
+  return (
+    <div>
+      <div>
+        <h1>Em Construção</h1>
+      </div>
+      <div>
+        <p>
+          Esse projeto tem o Intuito de criar uma comunidade brasileira de
+          Valorant, aonde os jogadores ajudam uns aos outros a evoluir, seja
+          como se comportar dentro do jogo, melhorar mira, pixels e enfins!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
