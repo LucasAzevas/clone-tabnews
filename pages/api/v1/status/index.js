f4c7ef089e41e31ac6de3d6ee407477e7d9593ba
Lucas Azevedo <85646897+LucasAzevas@users.noreply.github.com>
2024-11-08 05:06:53 +0000

    adiconando variaveis de ambiente e arquivo .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import query from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await query("SELECT 1 + 1 as SUM");
+  console.log(result);
   response.status(200).json({ response: "testando" });
 }
 
